152543a53e5b015d2017423ce0c88af6598b82e4
Akash <akashuxui@gmail.com>
2026-05-26 20:14:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.memoinfotech.bgwrap"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.bgremover.vqzpt"
+    applicationId = "com.memoinfotech.bgwrap"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
